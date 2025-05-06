14d64154297dd02fdfde0451b7d86383789251b1
kimdahee0815 <billionairedahee@gmail.com>
2025-05-06 15:15:08 +0900

    fix: cors setup

--- backend/src/main/java/com/mysite/weddingyou_backend/WebConfig.java
+++ backend/src/main/java/com/mysite/weddingyou_backend/WebConfig.java
@@ -11,7 +11,11 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-        .allowedOriginPatterns("https://weddingyou-dahee-kim.netlify.app", "http://localhost:*")
+            .allowedOrigins(
+                "https://weddingyou-dahee-kim.netlify.app",
+                "http://localhost:3000",
+                "http://localhost:80"
+            )
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .exposedHeaders("*")
