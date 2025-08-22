8359f2781f3e184315615bd64a1e31c560bbfd1f
kimdahee0815 <billionairedahee@gmail.com>
2025-08-22 19:37:46 +0900

    fix: qna search lower case

--- backend/src/main/java/com/mysite/weddingyou_backend/qna/QnaRepository.java
+++ backend/src/main/java/com/mysite/weddingyou_backend/qna/QnaRepository.java
@@ -3,10 +3,12 @@ package com.mysite.weddingyou_backend.qna;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QnaRepository extends JpaRepository<Qna, Long> {
+    @Query("SELECT q FROM Qna q WHERE LOWER(q.qnaTitle) LIKE LOWER(CONCAT('%', :keyword, '%'))")
     List<Qna> findByQnaTitleContaining(String keyword);
     List<Qna> findAllByQnaWriter(String email);
 }
