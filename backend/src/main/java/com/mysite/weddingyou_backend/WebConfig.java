01a97e887ebb9b838d640f63a392a7d4eef7e521
kimdahee0815 <kimdahee0815@gmail.com>
2026-06-30 15:37:12 +0900

    feat: converter for string

--- backend/src/main/java/com/mysite/weddingyou_backend/WebConfig.java
+++ backend/src/main/java/com/mysite/weddingyou_backend/WebConfig.java
@@ -1,13 +1,30 @@
 package com.mysite.weddingyou_backend;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.mysite.weddingyou_backend.item.StringToCategory1Converter;
+import com.mysite.weddingyou_backend.item.StringToCategory2Converter;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-	
+
+	@Autowired
+	private StringToCategory1Converter stringToCategory1Converter;
+
+	@Autowired
+	private StringToCategory2Converter stringToCategory2Converter;
+
+	@Override
+	public void addFormatters(FormatterRegistry registry) {
+		registry.addConverter(stringToCategory1Converter);
+		registry.addConverter(stringToCategory2Converter);
+	}
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
